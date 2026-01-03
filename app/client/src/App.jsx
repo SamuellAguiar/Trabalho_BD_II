43136ec5ea647c4ebdfa610ab97f71ae0384607f
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2026-01-02 23:56:26 -0300

    Initial client setup

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
+import AppRoutes from './routes/AppRoutes';
 
+function App() {
   return (
-    <>
-    </> 
-  )
+    <div className="app-main">
+      <AppRoutes />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- app/client/src/main.jsx
+++ app/client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <--- Importante
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* Envolve o App aqui */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
